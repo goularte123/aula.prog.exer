d849397b722a377dae7fea1628e554e553f10dc8
Jean Vitor <2002jeanvitorjv@gmail.com>
2022-03-25 21:54:58 -0300

    exercicios 7 8 9

---
+++ exercicios/Exercicio.Nove/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio.Nove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Livro("Teste", "Teste", 99m).ToString());
+            Console.ReadLine();
+        }
+    }
+}
